7a3d7e4e6a2c03a3c23c4f2cb33509e7b8eb82e7
Olof Hellqvist <olof.hellqvist@gmail.com>
2020-12-31 05:54:18 +0100

    Fixing

--- src/aiocasambi/__main__.py
+++ src/aiocasambi/__main__.py
@@ -127,8 +127,8 @@ async def main(*, email, user_password, network_password, api_key, wire_id=1, ss
 
             units = network_state_data['units']
 
-            for unit in units:
-                LOGGER.info(f"unit: {pprint.pformat(unit)}")
+            for unit_key, unit_data in units:
+                LOGGER.info(f"unit_key: {unit_key}\nunit_data: {pprint.pformat(unit_data)}")
 
             if controller.get_websocket_state() == 'disconnected':
                 await controller.reconnect()

537b2897efce31376185f8c53d63382e5bed14b6
Olof Hellqvist <olof.hellqvist@gmail.com>
2021-02-06 11:18:39 +0100

    Need this file

---
+++ src/aiocasambi/consts.py
@@ -0,0 +1,8 @@
+SIGNAL_DATA = 'data'
+SIGNAL_CONNECTION_STATE = 'state'
+SIGNAL_UNIT_PULL_UPDATE = 'unit pull update'
+
+STATE_DISCONNECTED = 'disconnected'
+STATE_RUNNING = 'running'
+STATE_STARTING = 'starting'
+STATE_STOPPED = 'stopped'

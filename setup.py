9cf55da78c7906f91f6f2011fe72f85c210a0318
Olof Hellqvist <olof.hellqvist@gmail.com>
2021-08-27 05:24:41 +0200

    Bumping up library

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='aiocasambi',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    version='0.212',
+    version='0.213',
     license='MIT',
     description='aio library to control Casambi light through cloudapi',
     long_description=long_description,
